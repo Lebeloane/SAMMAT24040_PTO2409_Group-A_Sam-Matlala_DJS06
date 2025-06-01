c15b1ade4b92e16f02651179e049a4bd5bac2fdd
Lebeloane <lebeloane@tutamail.com>
2025-06-01 21:17:00 +0200

    Console logging the imported scripts

---
+++ scripts.js
@@ -0,0 +1,24 @@
+import forEachExercises from './forEach.js';
+import mapExercises from './map.js';
+import sortFilterExercises from './sortFilter.js';
+import reduceExercises from './reduce.js';
+import advancedExercises from './advanced.js';
+
+
+console.log('Running forEach Exercises');
+forEachExercises();
+
+
+console.log('Map Exercises');
+mapExercises();
+
+
+console.log('Sort and Filter Exercises');
+sortFilterExercises();
+
+
+console.log('Reduce Exercises');
+reduceExercises();
+
+console.log('Advanced Exercises');
+advancedExercises();
